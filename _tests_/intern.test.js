2209c28627e78842698adde8dad79029d71fb9c2
Taegan Loyzelle <tloyzelle@gmail.com>
2021-07-08 21:52:02 -0400

    adding intern.test.js

---
+++ _tests_/intern.test.js
@@ -0,0 +1,19 @@
+const intern = require("../lib/intern");
+
+it("can create school through the constructor", () => {
+    const testValue = "UNCC";
+    const e = new intern("Maddy", 1, "maddy@example.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+it("can use getRole to return \"intern\"", () => {
+    const testValuye = "intern";
+    const e = new intern("Maddy", 1, "maddy@example.com", "UNCC");
+    expect(e.getRole()).toBe(testValue);
+});
+
+it("can create school from getSchool()", () => {
+    const testValue = "UNCC";
+    const e = new intern("Maddy", 1, "maddy@example.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
